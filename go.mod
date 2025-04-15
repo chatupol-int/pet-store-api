727ef3a510efeee5e12b106bb21d605b6beb8816
chatupol-int <chatupol.int@gmail.com>
2025-04-15 09:03:35 +0700

    feat: add a simple api

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pet-store-api
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	ID      int    `json:"id"`
+	Content string `json:"content"`
+}
+
+var messages = []Message{
+	{ID: 1, Content: "Hello, World!"},
+	{ID: 2, Content: "Golang is awesome!"},
+}
+
+func main() {
+	http.HandleFunc("/api/messages", handleMessages)
+	log.Println("🚀 Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleMessages(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case http.MethodGet:
+		json.NewEncoder(w).Encode(messages)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
